171952ef7d4da69f8607dafbcffddbdff0174082
Mr-Leshiy <leshiy12345678@gmail.com>
2022-06-18 15:00:31 +0300

    add parse implementation for Function ast

--- src/ast/function_declaration.rs
+++ src/ast/function_declaration.rs
@@ -1,6 +1,6 @@
 use super::{BlockStatement, Identifier};
 use crate::{
-    lexer::{CharReader, Token},
+    lexer::{CharReader, Separator, Token},
     parser::{self, Parser},
 };
 use std::io::Read;
@@ -13,9 +13,61 @@ pub struct FunctionDeclaration {
 }
 
 impl Parser for FunctionDeclaration {
-    fn parse<R: Read>(cur_token: Token, _: &mut CharReader<R>) -> Result<Self, parser::Error> {
+    fn parse<R: Read>(
+        mut cur_token: Token,
+        reader: &mut CharReader<R>,
+    ) -> Result<Self, parser::Error> {
         match cur_token {
+            Token::Function => {
+                // parse function name
+                let name = Identifier::parse(Token::get_token(reader)?, reader)?;
+
+                // parse function args
+                let args = match Token::get_token(reader)? {
+                    Token::Separator(Separator::OpenBrace) => {
+                        let mut args = Vec::new();
+                        cur_token = Token::get_token(reader)?;
+                        loop {
+                            let arg = match cur_token {
+                                Token::Separator(Separator::CloseBrace) => break,
+                                cur_token => Identifier::parse(cur_token, reader)?,
+                            };
+
+                            cur_token = Token::get_token(reader)?;
+                            args.push(arg);
+                        }
+                        Ok(args)
+                    }
+                    token => Err(parser::Error::UnexpectedToken(token)),
+                }?;
+
+                // parse function body
+                let body = BlockStatement::parse(Token::get_token(reader)?, reader)?;
+
+                Ok(Self { name, args, body })
+            }
             token => Err(parser::Error::UnexpectedToken(token)),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn function_declaration_test() {
+        let mut reader = CharReader::new("function foo () {}".as_bytes());
+        assert_eq!(
+            FunctionDeclaration::parse(Token::get_token(&mut reader).unwrap(), &mut reader)
+                .unwrap(),
+            FunctionDeclaration {
+                name: Identifier {
+                    name: "foo".to_string()
+                },
+                args: vec![],
+                body: BlockStatement { body: vec![] }
+            }
+        );
     }
 }
