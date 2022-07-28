51a6557cc4eb477fbc205bd838d59bf8455b4221
Mr-Leshiy <leshiy12345678@gmail.com>
2022-07-28 21:19:40 +0300

    update variable.js test

--- test_scripts/basic/variables.js
+++ test_scripts/basic/variables.js
@@ -1,15 +1,35 @@
 var a1 = 5;
-var a1 = 12;
-var a2 = "Hello world";
-var a3 = true;
-var a4 = false;
+assert_eq(a1, 5);
 
 var a1 = 12;
+assert_eq(a1, 12);
+
+var a2 = 12;
+assert_eq(a1, a2);
+
+var a2 = "Hello world";
+
+var a3 = true;
+assert(a3);
+assert_eq(a3, true);
+
+var a4 = false;
+assert_eq(a4, false);
 
 a3 = 6.421;
-a1 = "Hi";
+assert_eq(a3, 6.421);
+
 a3 = a3;
+assert_eq(a3, 6.421);
+
 a3 = a1;
+assert_eq(a3, a1);
 
 var a5 = a4;
+assert_eq(a5, a4);
+
 a5 = 0;
+assert_eq(a5, 0);
+
+a4 = 0;
+assert_eq(a5, a4);
