b389d721a89c3815d983011bbebbb63f9add82e7
Mr-Leshiy <leshiy12345678@gmail.com>
2022-09-01 22:39:03 +0300

    implement number case

--- compiler/src/logical_operations/mod.rs
+++ compiler/src/logical_operations/mod.rs
@@ -6,8 +6,46 @@ pub fn logical_not<'ctx, T>(
     arg: Variable<'ctx>,
 ) -> Result<Variable<'ctx>, Error<T>> {
     let arg = Variable::new_variable(compiler, cur_function, &arg);
-    let number_case_f = |_compiler: &Compiler<'ctx, T>| {
-        // TODO implement
+    let number_case_f = |compiler: &Compiler<'ctx, T>| {
+        let number_field = arg.get_field(compiler, Field::Number);
+        let number_field = compiler
+            .builder
+            .build_load(number_field, "")
+            .into_float_value();
+
+        let true_block = compiler
+            .context
+            .append_basic_block(cur_function.function, "");
+        let false_block = compiler
+            .context
+            .append_basic_block(cur_function.function, "");
+        let continue_block = compiler
+            .context
+            .append_basic_block(cur_function.function, "");
+
+        let comparison = compiler.builder.build_float_compare(
+            inkwell::FloatPredicate::OEQ,
+            number_field,
+            compiler.context.f64_type().const_float(0_f64),
+            "",
+        );
+
+        compiler
+            .builder
+            .build_conditional_branch(comparison, true_block, false_block);
+
+        // describe false case
+        compiler.builder.position_at_end(false_block);
+        arg.assign_boolean(compiler, false);
+        compiler.builder.build_unconditional_branch(continue_block);
+
+        // describe true case
+        compiler.builder.position_at_end(true_block);
+        arg.assign_boolean(compiler, true);
+        compiler.builder.build_unconditional_branch(continue_block);
+
+        //
+        compiler.builder.position_at_end(continue_block);
     };
     let string_case_f = |_compiler: &Compiler<'ctx, T>| {
         // TODO implement
--- test_scripts/basic/logical_not.js
+++ test_scripts/basic/logical_not.js
@@ -1,6 +1,4 @@
 var a = true;
-
-assert_eq(a, true);
 assert_eq(!a, false);
 assert_eq(!!a, true);
 assert_eq(!!!a, false);
@@ -8,7 +6,27 @@ assert_eq(!!!!a, true);
 assert_eq(!!!!!a, false);
 
 var a = false;
-assert_eq(a, false);
+assert_eq(!a, true);
+assert_eq(!!a, false);
+assert_eq(!!!a, true);
+assert_eq(!!!!a, false);
+assert_eq(!!!!!a, true);
+
+var a = 5;
+assert_eq(!a, false);
+assert_eq(!!a, true);
+assert_eq(!!!a, false);
+assert_eq(!!!!a, true);
+assert_eq(!!!!!a, false);
+
+var a = -5;
+assert_eq(!a, false);
+assert_eq(!!a, true);
+assert_eq(!!!a, false);
+assert_eq(!!!!a, true);
+assert_eq(!!!!!a, false);
+
+var a = 0;
 assert_eq(!a, true);
 assert_eq(!!a, false);
 assert_eq(!!!a, true);
