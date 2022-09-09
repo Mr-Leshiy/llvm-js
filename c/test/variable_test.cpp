6bda5e5c8a9ac98ac1b7a94e5692e052780b1af3
Mr-Leshiy <leshiy12345678@gmail.com>
2022-09-09 23:47:13 +0300

    add basic variables typing

--- c/test/variable_test.cpp
+++ c/test/variable_test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <string.h>
 
 extern "C" {
 #include "variable.h"
@@ -6,6 +7,32 @@ extern "C" {
 
 TEST(VariableType, Basic_test)
 {
-    VariableType* var = allocate();
-    EXPECT_TRUE(true);
+    VariableType* var1 = allocate();
+    VariableType* var2 = allocate();
+
+    EXPECT_NE(var1, nullptr);
+    EXPECT_NE(var2, nullptr);
+    EXPECT_EQ(var1->flag, 0);
+    EXPECT_EQ(var2->flag, 0);
+
+    set_number(var1, 2.0);
+    set_variable(var2, var1);
+    EXPECT_EQ(var1->flag, Number);
+    EXPECT_EQ(var2->flag, Number);
+    EXPECT_EQ(var1->number_field, 2.0);
+    EXPECT_EQ(var2->number_field, 2.0);
+
+    set_boolean(var1, true);
+    set_variable(var2, var1);
+    EXPECT_EQ(var1->flag, Boolean);
+    EXPECT_EQ(var2->flag, Boolean);
+    EXPECT_EQ(var1->boolean_field, true);
+    EXPECT_EQ(var2->boolean_field, true);
+
+    set_string(var1, "foo");
+    set_variable(var2, var1);
+    EXPECT_EQ(var1->flag, String);
+    EXPECT_EQ(var2->flag, String);
+    EXPECT_EQ(strcmp(var1->string_field, "foo"), 0);
+    EXPECT_EQ(strcmp(var2->string_field, "foo"), 0);
 }
--- c/variable.h
+++ c/variable.h
@@ -8,11 +8,20 @@ typedef struct VariableType
     uint8_t flag;
     double number_field;
     uint8_t boolean_field;
-
+    const char* string_field;
 } VariableType;
+
+enum Type {
+    Number = 1,
+    Boolean = 2,
+    String = 3,
+};
 
 VariableType *allocate();
 
-void foo();
+void set_number(VariableType* self, double val);
+void set_boolean(VariableType* self, uint8_t val);
+void set_string(VariableType* self, const char* val);
+void set_variable(VariableType* self, VariableType* val);
 
 #endif
