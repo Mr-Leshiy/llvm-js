039148be0587f50d8eb4d6330cd1022ad9039011
Mr-Leshiy <leshiy12345678@gmail.com>
2022-09-10 14:54:29 +0300

    add logical_not impl

--- c/logical.c
+++ c/logical.c
@@ -1,1 +1,50 @@
+#include <assert.h>
+#include <stdlib.h>
+
 #include "logical.h"
+
+VariableType* logical_not(VariableType* val)
+{
+    assert(val != NULL);
+
+    VariableType* ret = convert_to_boolean(val);
+    if(ret->boolean_field)
+    {
+        ret->boolean_field = 0;
+    }
+    else
+    {
+        ret->boolean_field = 1;
+    }
+    return ret;
+}
+
+VariableType* logical_and(VariableType* val1, VariableType* val2)
+{
+
+}
+
+VariableType* logical_or(VariableType* val1, VariableType* val2)
+{
+
+}
+
+VariableType* logical_eq(VariableType* val1, VariableType* val2)
+{
+
+}
+
+VariableType* logical_ne(VariableType* val1, VariableType* val2)
+{
+
+}
+
+VariableType* logical_seq(VariableType* val1, VariableType* val2)
+{
+
+}
+
+VariableType* logical_sne(VariableType* val1, VariableType* val2)
+{
+
+}
--- c/test/logical_test.cpp
+++ c/test/logical_test.cpp
@@ -5,6 +5,38 @@ extern "C" {
 #include "logical.h"
 }
 
-TEST(Logical, Basic_test)
+TEST(Logical, logical_not_test)
 {
+    VariableType* res;
+    VariableType* val = allocate();
+
+    set_boolean(val, true);
+    res = logical_not(val);
+    EXPECT_EQ(res->flag, Boolean);
+    EXPECT_EQ(res->boolean_field, false);
+
+    set_boolean(val, false);
+    res = logical_not(val);
+    EXPECT_EQ(res->flag, Boolean);
+    EXPECT_EQ(res->boolean_field, true);
+
+    set_number(val, 2.5);
+    res = logical_not(val);
+    EXPECT_EQ(res->flag, Boolean);
+    EXPECT_EQ(res->boolean_field, false);
+
+    set_number(val, 0);
+    res = logical_not(val);
+    EXPECT_EQ(res->flag, Boolean);
+    EXPECT_EQ(res->boolean_field, true);
+
+    set_string(val, "Hello world");
+    res = logical_not(val);
+    EXPECT_EQ(res->flag, Boolean);
+    EXPECT_EQ(res->boolean_field, false);
+
+    set_string(val, "");
+    res = logical_not(val);
+    EXPECT_EQ(res->flag, Boolean);
+    EXPECT_EQ(res->boolean_field, true);
 }
