2867896c8582733c9dde9045738ad9b167925507
Mr-Leshiy <leshiy12345678@gmail.com>
2022-09-16 21:26:08 +0300

    update function test

--- test_scripts/basic/functions.js
+++ test_scripts/basic/functions.js
@@ -7,18 +7,18 @@ function foo(arg1, arg2) {
 var a = 4;
 var b = 3;
 
-foo(a, b);
-foo(4, true);
-foo("hello", a);
+assert_eq(foo(a, b), undefined);
+assert_eq(foo(4, true), undefined);
+assert_eq(foo("hello", a), undefined);
 
 assert_eq(a, 4);
 assert_eq(b, 3);
 
-function foo(arg1, arg2) {}
+function foo(arg1, arg2) {return 5;}
 
-foo(a, b);
-foo(4, true);
-foo("hello", a);
+assert_eq(foo(a, b), 5);
+assert_eq(foo(4, true), 5);
+assert_eq(foo("hello", a), 5);
 
 var res = foo(4, 5);
-assert_eq(res, undefined);
+assert_eq(res, 5);
