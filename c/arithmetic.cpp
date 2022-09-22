b9cfe452426d3610da8f5c49bcc670e43be56227
Mr-Leshiy <leshiy12345678@gmail.com>
2022-09-22 10:43:04 +0300

    fix multiplication and add division, update tests

--- c/arithmetic.cpp
+++ c/arithmetic.cpp
@@ -158,6 +158,110 @@ Variable *arithmetic_multiplication(Variable *val1, Variable *val2)
     {
         set_nan(ret);
     }
+    else if (val1->flag == Type::Infinity)
+    {
+        if (val2->flag == Type::NegInfinity)
+        {
+            set_neginfinity(ret);
+        }
+        else if (val2->flag == Type::Infinity)
+        {
+            set_infinity(ret);
+        }
+        else
+        {
+            if (val2->number_field == 0)
+            {
+                set_nan(ret);
+            }
+            else if (val2->number_field > 0)
+            {
+                set_infinity(ret);
+            }
+            else
+            {
+                set_neginfinity(ret);
+            }
+        }
+    }
+    else if (val2->flag == Type::Infinity)
+    {
+        if (val1->flag == Type::NegInfinity)
+        {
+            set_neginfinity(ret);
+        }
+        else if (val1->flag == Type::Infinity)
+        {
+            set_infinity(ret);
+        }
+        else
+        {
+            if (val1->number_field == 0)
+            {
+                set_nan(ret);
+            }
+            else if (val1->number_field > 0)
+            {
+                set_infinity(ret);
+            }
+            else
+            {
+                set_neginfinity(ret);
+            }
+        }
+    }
+    else if (val1->flag == Type::NegInfinity)
+    {
+        if (val2->flag == Type::NegInfinity)
+        {
+            set_infinity(ret);
+        }
+        else if (val2->flag == Type::Infinity)
+        {
+            set_neginfinity(ret);
+        }
+        else
+        {
+            if (val2->number_field == 0)
+            {
+                set_nan(ret);
+            }
+            else if (val2->number_field > 0)
+            {
+                set_neginfinity(ret);
+            }
+            else
+            {
+                set_infinity(ret);
+            }
+        }
+    }
+    else if (val2->flag == Type::NegInfinity)
+    {
+        if (val1->flag == Type::NegInfinity)
+        {
+            set_infinity(ret);
+        }
+        else if (val1->flag == Type::Infinity)
+        {
+            set_neginfinity(ret);
+        }
+        else
+        {
+            if (val1->number_field == 0)
+            {
+                set_nan(ret);
+            }
+            else if (val1->number_field > 0)
+            {
+                set_neginfinity(ret);
+            }
+            else
+            {
+                set_infinity(ret);
+            }
+        }
+    }
     else
     {
         set_number(ret, val1->number_field * val2->number_field);
@@ -178,9 +282,82 @@ Variable *arithmetic_division(Variable *val1, Variable *val2)
     {
         set_nan(ret);
     }
+    else if (val1->flag == Type::Infinity)
+    {
+        if (val2->flag == Type::NegInfinity || val2->flag == Type::Infinity)
+        {
+            set_nan(ret);
+        }
+        else
+        {
+            if (val2->number_field >= 0)
+            {
+                set_infinity(ret);
+            }
+            else
+            {
+                set_neginfinity(ret);
+            }
+        }
+    }
+    else if (val2->flag == Type::Infinity)
+    {
+        if (val1->flag == Type::NegInfinity || val1->flag == Type::Infinity)
+        {
+            set_nan(ret);
+        }
+        else
+        {
+            set_number(ret, 0);
+        }
+    }
+    else if (val1->flag == Type::NegInfinity)
+    {
+        if (val2->flag == Type::NegInfinity || val2->flag == Type::Infinity)
+        {
+            set_nan(ret);
+        }
+        else
+        {
+            if (val2->number_field >= 0)
+            {
+                set_neginfinity(ret);
+            }
+            else
+            {
+                set_infinity(ret);
+            }
+        }
+    }
+    else if (val2->flag == Type::NegInfinity)
+    {
+        if (val1->flag == Type::NegInfinity || val1->flag == Type::Infinity)
+        {
+            set_nan(ret);
+        }
+        else
+        {
+            set_number(ret, 0);
+        }
+    }
     else
     {
-        set_number(ret, val1->number_field * val2->number_field);
+        if (val2->number_field != 0)
+        {
+            set_number(ret, val1->number_field / val2->number_field);
+        }
+        else if (val1->number_field == 0)
+        {
+            set_nan(ret);
+        }
+        else if (val1->number_field > 0)
+        {
+            set_infinity(ret);
+        }
+        else
+        {
+            set_neginfinity(ret);
+        }
     }
     return ret;
 }
--- c/test/arithmetic_test.cpp
+++ c/test/arithmetic_test.cpp
@@ -10,81 +10,96 @@ TEST(Logical, arithmetic_addition_test)
     Variable *val1 = allocate();
     Variable *val2 = allocate();
 
+    // 2 + 3 = 5
     set_number(val1, 2);
     set_number(val2, 3);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 5);
 
+    // 2 + 0 = 2
     set_number(val1, 2);
     set_number(val2, 0);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 2);
 
+    // 0 + 2 = 2
     set_number(val1, 0);
     set_number(val2, 2);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 2);
 
+    // -2 + 3 = 1
     set_number(val1, -2);
     set_number(val2, 3);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 1);
 
+    // -2.5 + -4.5 = -7
     set_number(val1, -2.5);
     set_number(val2, -4.5);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, -7);
 
+    // NaN + -4.5 = NaN
     set_nan(val1);
     set_number(val2, -4.5);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // -4.5 + NaN = NaN
     set_number(val1, -4.5);
     set_nan(val2);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // Infinity + NaN = NaN
     set_infinity(val1);
     set_nan(val2);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // -Infinity + NaN = NaN
     set_neginfinity(val1);
     set_nan(val2);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // Infinity + -4.5 = Infinity
     set_infinity(val1);
     set_number(val2, -4.5);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::Infinity);
 
+    // -Infinity + -4.5 = -Infinity
     set_neginfinity(val1);
     set_number(val2, -4.5);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::NegInfinity);
 
+    // Infinity + -Infinity = NaN
     set_infinity(val1);
     set_neginfinity(val2);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // -Infinity + Infinity = NaN
     set_neginfinity(val1);
     set_infinity(val2);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // Infinity + Infinity = Infinity
     set_infinity(val1);
     set_infinity(val2);
     res = arithmetic_addition(val1, val2);
     EXPECT_EQ(res->flag, Type::Infinity);
 
+    // -Infinity + -Infinity = -Infinity
     set_neginfinity(val1);
     set_neginfinity(val2);
     res = arithmetic_addition(val1, val2);
@@ -97,81 +112,96 @@ TEST(Logical, arithmetic_substraction_test)
     Variable *val1 = allocate();
     Variable *val2 = allocate();
 
+    // 2 - 3 = -1
     set_number(val1, 2);
     set_number(val2, 3);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, -1);
 
+    // 2 - 0 = 2
     set_number(val1, 2);
     set_number(val2, 0);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 2);
 
+    // 0 - 2 = -2
     set_number(val1, 0);
     set_number(val2, 2);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, -2);
 
+    // -2 - 3 = -5
     set_number(val1, -2);
     set_number(val2, 3);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, -5);
 
+    // -2.5 - -4.5 = 2
     set_number(val1, -2.5);
     set_number(val2, -4.5);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 2);
 
+    // NaN - -4.5 = NaN
     set_nan(val1);
     set_number(val2, -4.5);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // -4.5 - NaN = NaN
     set_number(val1, -4.5);
     set_nan(val2);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // Infinity - NaN = NaN
     set_infinity(val1);
     set_nan(val2);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // -Infinity - NaN = NaN
     set_neginfinity(val1);
     set_nan(val2);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // Infinity - -4.5 = Infinity
     set_infinity(val1);
     set_number(val2, -4.5);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::Infinity);
 
+    // -Infinity - -4.5 = -Infinity
     set_neginfinity(val1);
     set_number(val2, -4.5);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::NegInfinity);
 
+    // Infinity - -Infinity = Infinity
     set_infinity(val1);
     set_neginfinity(val2);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::Infinity);
 
+    // -Infinity - Infinity = -Infinity
     set_neginfinity(val1);
     set_infinity(val2);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::NegInfinity);
 
+    // Infinity - Infinity = NaN
     set_infinity(val1);
     set_infinity(val2);
     res = arithmetic_substraction(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // -Infinity - -Infinity = NaN
     set_neginfinity(val1);
     set_neginfinity(val2);
     res = arithmetic_substraction(val1, val2);
@@ -184,45 +214,160 @@ TEST(Logical, arithmetic_multiplication_test)
     Variable *val1 = allocate();
     Variable *val2 = allocate();
 
+    // 2 * 3 = 6
     set_number(val1, 2);
     set_number(val2, 3);
     res = arithmetic_multiplication(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 6);
 
+    // 2 * 0 = 0
     set_number(val1, 2);
     set_number(val2, 0);
     res = arithmetic_multiplication(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 0);
 
+    // 0 * 2 = 0
     set_number(val1, 0);
     set_number(val2, 2);
     res = arithmetic_multiplication(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 0);
 
+    // -2 * 3 = -6
     set_number(val1, -2);
     set_number(val2, 3);
     res = arithmetic_multiplication(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, -6);
 
+    // -2.5 * -4.5 = 11.25
     set_number(val1, -2.5);
     set_number(val2, -4.5);
     res = arithmetic_multiplication(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 11.25);
 
+    // NaN * -4.5 = NaN
     set_nan(val1);
     set_number(val2, -4.5);
     res = arithmetic_multiplication(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
 
+    // -4.5 * NaN = NaN
     set_number(val1, -4.5);
     set_nan(val2);
     res = arithmetic_multiplication(val1, val2);
     EXPECT_EQ(res->flag, Type::NaN);
+
+    // Infinity * NaN = NaN
+    set_infinity(val1);
+    set_nan(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // -Infinity * NaN = NaN
+    set_neginfinity(val1);
+    set_nan(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // Infinity * -4.5 = -Infinity
+    set_infinity(val1);
+    set_number(val2, -4.5);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NegInfinity);
+
+    // 4.5 * Infinity = Infinity
+    set_number(val1, 4.5);
+    set_infinity(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::Infinity);
+
+    // 4.5 * -Infinity = -Infinity
+    set_number(val1, 4.5);
+    set_neginfinity(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NegInfinity);
+
+    // Infinity * 4.5 = Infinity
+    set_infinity(val1);
+    set_number(val2, 4.5);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::Infinity);
+
+    // -Infinity * 4.5 = -Infinity
+    set_neginfinity(val1);
+    set_number(val2, 4.5);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NegInfinity);
+
+    // -4.5 * Infinity = -Infinity
+    set_number(val1, -4.5);
+    set_infinity(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NegInfinity);
+
+    // -Infinity * -4.5 = Infinity
+    set_neginfinity(val1);
+    set_number(val2, -4.5);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::Infinity);
+
+    // -4.5 * -Infinity = Infinity
+    set_number(val1, -4.5);
+    set_neginfinity(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::Infinity);
+
+    // 0 * -Infinity = NaN
+    set_number(val1, 0);
+    set_neginfinity(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // 0 * Infinity = NaN
+    set_number(val1, 0);
+    set_infinity(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // -Infinity * 0 = NaN
+    set_neginfinity(val1);
+    set_number(val2, 0);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // Infinity * 0 = NaN
+    set_infinity(val1);
+    set_number(val2, 0);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // Infinity * -Infinity = -Infinity
+    set_infinity(val1);
+    set_neginfinity(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NegInfinity);
+
+    // -Infinity * Infinity = -Infinity
+    set_neginfinity(val1);
+    set_infinity(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::NegInfinity);
+
+    // Infinity * Infinity = Infinity
+    set_infinity(val1);
+    set_infinity(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::Infinity);
+
+    // -Infinity * -Infinity = Infinity
+    set_neginfinity(val1);
+    set_neginfinity(val2);
+    res = arithmetic_multiplication(val1, val2);
+    EXPECT_EQ(res->flag, Type::Infinity);
 }
 
 TEST(Logical, arithmetic_division_test)
@@ -231,43 +376,123 @@ TEST(Logical, arithmetic_division_test)
     Variable *val1 = allocate();
     Variable *val2 = allocate();
 
-    set_number(val1, 2);
+    // 6 / 3 = 2
+    set_number(val1, 6);
     set_number(val2, 3);
-    res = arithmetic_multiplication(val1, val2);
+    res = arithmetic_division(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
-    EXPECT_EQ(res->number_field, 6);
+    EXPECT_EQ(res->number_field, 2);
 
-    set_number(val1, 2);
+    // -6 / 3 = -2
+    set_number(val1, -6);
+    set_number(val2, 3);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::Number);
+    EXPECT_EQ(res->number_field, -2);
+
+    // 6 / -3 = -2
+    set_number(val1, 6);
+    set_number(val2, -3);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::Number);
+    EXPECT_EQ(res->number_field, -2);
+
+    // -6 / -3 = -2
+    set_number(val1, -6);
+    set_number(val2, -3);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::Number);
+    EXPECT_EQ(res->number_field, 2);
+
+    // 3 / 0 = Infinity
+    set_number(val1, 3);
     set_number(val2, 0);
-    res = arithmetic_multiplication(val1, val2);
-    EXPECT_EQ(res->flag, Type::Number);
-    EXPECT_EQ(res->number_field, 0);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::Infinity);
 
+    // -3 / 0 = -Infinity
+    set_number(val1, -3);
+    set_number(val2, 0);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::NegInfinity);
+
+    // 0 / 0 = NaN
     set_number(val1, 0);
+    set_number(val2, 0);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // NaN / 2 = NaN
+    set_nan(val1);
     set_number(val2, 2);
-    res = arithmetic_multiplication(val1, val2);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // 2 / NaN = NaN
+    set_number(val1, 2);
+    set_nan(val2);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // Infinity / NaN = NaN
+    set_infinity(val1);
+    set_nan(val2);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // -Infinity / NaN = NaN
+    set_neginfinity(val1);
+    set_nan(val2);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // NaN / Infinity = NaN
+    set_nan(val1);
+    set_infinity(val2);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // NaN / -Infinity = NaN
+    set_nan(val1);
+    set_neginfinity(val2);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::NaN);
+
+    // Infinity / 1 = Infinity
+    set_infinity(val1);
+    set_number(val2, 1);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::Infinity);
+
+    // -Infinity / 1 = -Infinity
+    set_neginfinity(val1);
+    set_number(val2, 1);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::NegInfinity);
+
+    // Infinity / -1 = -Infinity
+    set_infinity(val1);
+    set_number(val2, -1);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::NegInfinity);
+
+    // -Infinity / -1 = Infinity
+    set_neginfinity(val1);
+    set_number(val2, -1);
+    res = arithmetic_division(val1, val2);
+    EXPECT_EQ(res->flag, Type::Infinity);
+
+    // 1 / Infinity = 0
+    set_number(val1, 1);
+    set_infinity(val2);
+    res = arithmetic_division(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
     EXPECT_EQ(res->number_field, 0);
 
-    set_number(val1, -2);
-    set_number(val2, 3);
-    res = arithmetic_multiplication(val1, val2);
+    // 1 / -Infinity = 0
+    set_number(val1, 1);
+    set_neginfinity(val2);
+    res = arithmetic_division(val1, val2);
     EXPECT_EQ(res->flag, Type::Number);
-    EXPECT_EQ(res->number_field, -6);
-
-    set_number(val1, -2.5);
-    set_number(val2, -4.5);
-    res = arithmetic_multiplication(val1, val2);
-    EXPECT_EQ(res->flag, Type::Number);
-    EXPECT_EQ(res->number_field, 11.25);
-
-    set_nan(val1);
-    set_number(val2, -4.5);
-    res = arithmetic_multiplication(val1, val2);
-    EXPECT_EQ(res->flag, Type::NaN);
-
-    set_number(val1, -4.5);
-    set_nan(val2);
-    res = arithmetic_multiplication(val1, val2);
-    EXPECT_EQ(res->flag, Type::NaN);
+    EXPECT_EQ(res->number_field, 0);
 }
