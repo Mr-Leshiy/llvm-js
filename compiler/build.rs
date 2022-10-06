25db8d6e3ec7c3acb7f0e78469c6744634c56f5b
Mr-Leshiy <leshiy12345678@gmail.com>
2022-10-06 21:12:41 +0300

    fix

--- compiler/build.rs
+++ compiler/build.rs
@@ -1,7 +1,6 @@
 fn main() {
     let cmake = cmake::Config::new("..")
         .out_dir("..")
-        .define("TESTING", "OFF")
         .no_build_target(true)
         .build();
     println!("{}", cmake.display());
