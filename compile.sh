d75bc65671471baddc9ca3f894dd65f855658387
Alex Pozhylenkov <leshiy12345678@gmail.com>
2022-10-07 13:56:27 +0300

    Update build (#87)

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 llc -o out.s $1
-clang++ -L c/out/ -l llvm-js -o test_run out.s
+clang++ -L build/lib/ -l llvm-js -o test_run out.s
 rm out.s
--- compiler/build.rs
+++ compiler/build.rs
@@ -1,12 +1,7 @@
 fn main() {
-    cc::Build::new()
-        .cpp(true)
-        .flag("-std=c++17")
-        .file("../c/variable.cpp")
-        .file("../c/logical.cpp")
-        .file("../c/assertions.cpp")
-        .file("../c/arithmetic.cpp")
-        .out_dir("../c/out")
-        .cargo_metadata(false)
-        .compile("llvm-js")
+    let cmake = cmake::Config::new("..")
+        .out_dir("../c")
+        .no_build_target(true)
+        .build();
+    println!("{}", cmake.display());
 }
