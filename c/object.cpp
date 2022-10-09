64a2669327b7e5a1ccab6708491259926a171b59
Mr-Leshiy <leshiy12345678@gmail.com>
2022-10-09 19:13:28 +0300

    fix object to_string method

--- c/object.cpp
+++ c/object.cpp
@@ -41,7 +41,7 @@ std::string Object::to_string() const
     std::string res = "{";
     for (const auto &el : this->properties)
     {
-        res += el.first + ": " + el.second->to_string();
+        res += " " + el.first + ": " + el.second->to_string() + " ";
     }
     res += "}";
     return res;
