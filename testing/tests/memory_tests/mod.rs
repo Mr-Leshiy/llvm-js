16350ed2e2067a7c304e6917004abe53e8556d7d
Mr-Leshiy <leshiy12345678@gmail.com>
2023-01-21 18:22:42 +0200

    fix fmt

--- testing/tests/memory_tests/mod.rs
+++ testing/tests/memory_tests/mod.rs
@@ -1,14 +1,11 @@
 use crate::CompileSuite;
 
 #[test]
-fn primitive_types_test() {
-    CompileSuite::new(
-        "../test_scripts/memory/basic.js",
-        "basic",
-    )
-    .compile()
-    .unwrap()
-    .run_with_valgrind()
-    .unwrap()
-    .cleanup();
+fn basic_test() {
+    CompileSuite::new("../test_scripts/memory/basic.js", "basic")
+        .compile()
+        .unwrap()
+        .run_with_valgrind()
+        .unwrap()
+        .cleanup();
 }
