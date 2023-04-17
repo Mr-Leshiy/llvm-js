9c76a18fc326d2eafb6ad550a491aa75321d7209
Mr-Leshiy <leshiy12345678@gmail.com>
2023-04-17 21:15:59 +0300

    refactor object

--- core/src/object.rs
+++ core/src/object.rs
@@ -2,39 +2,52 @@ use crate::{pointer::Ptr, variable::Variable};
 use std::collections::HashMap;
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Object {
-    properties: HashMap<String, Ptr<Variable>>,
+pub enum Object {
+    Object {
+        properties: HashMap<String, Ptr<Variable>>,
+    },
 }
 
 impl Object {
     pub fn to_string(&self) -> String {
-        let mut res = "{".to_string();
-        for (property_name, property) in &self.properties {
-            res.push_str(
-                format!("{0}: {1}", property_name, property.get_ref().to_string()).as_str(),
-            );
+        let mut res = String::new();
+        match self {
+            Self::Object { properties } => {
+                res.push('{');
+                for (property_name, property) in properties {
+                    res.push_str(
+                        format!("{0}: {1}", property_name, property.get_ref().to_string()).as_str(),
+                    );
+                }
+                res.push('}');
+            }
         }
-        res.push('}');
         res
     }
 }
 
 impl Object {
-    pub fn new() -> Self {
-        Self {
+    pub fn new_object() -> Self {
+        Self::Object {
             properties: HashMap::new(),
         }
     }
 
     pub fn add_property(&mut self, property_name: String, property: Ptr<Variable>) {
-        self.properties.insert(property_name, property);
+        match self {
+            Self::Object { properties } => {
+                properties.insert(property_name, property);
+            }
+        }
     }
 
     pub fn get_property(&self, property_name: String) -> Ptr<Variable> {
-        self.properties
-            .get(&property_name)
-            .map(|el| el.copy())
-            .unwrap_or_else(|| Ptr::allocate(Variable::Undefined))
+        match self {
+            Self::Object { properties } => properties
+                .get(&property_name)
+                .map(|el| el.copy())
+                .unwrap_or_else(|| Ptr::allocate(Variable::Undefined)),
+        }
     }
 }
 
@@ -44,7 +57,7 @@ mod tests {
 
     #[test]
     fn basic_test() {
-        let mut object = Object::new();
+        let mut object = Object::new_object();
         assert_eq!(object.to_string(), "{}".to_string());
 
         assert_eq!(
